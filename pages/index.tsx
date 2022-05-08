87ee72ba63fdfaa236e6ec05bf300f56f45d4274
Peperman66 <peperman66@seznam.cz>
2022-05-08 21:40:01 +0200

    add better look to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { FC, useState } from 'react'
 import { StationData } from '../types/stationData'
 
 const Home: NextPage = () => {
@@ -13,9 +13,17 @@ const Home: NextPage = () => {
   }
   return (
     <>
-      <button onClick={getRandomStation}>Vybrat stanici</button>
-      <span>Vybraná stanice:</span>
-      <span>{station?.name}</span>
+      <div className='flex flex-col items-center'>
+        <h1 className="text-6xl pb-4">Výběr náhodné zastávky v Praze</h1>
+        <hr className='border-black w-full border-2' />
+        <button className={'bg-gray-300 rounded-lg border p-1 text-3xl mt-4' + (inProgress ? " text-gray-100" : "")} onClick={getRandomStation} disabled={inProgress}>{inProgress ? "Vybírám..." : "Vybrat zastávku"}</button>
+        <span className='text-xl'>Vybraná zastávka:</span>
+        <span className='font-bold text-2xl'>{station?.name}</span>
+        {station != undefined && <>
+          <a className='text-blue-700 underline' href={getMapUrl()} target="_blank">Odkaz na zastávku</a>
+          <CustomMap center={center}/>
+        </>}
+      </div>
     </>
   )
 }
