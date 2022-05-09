26d408ecaeacec3df0d084ef67eff9f5cb3f2859
Peperman66 <peperman66@seznam.cz>
2022-05-09 22:03:36 +0200

    add parse-osm to separate file

---
+++ scripts/parse-osm.ts
@@ -0,0 +1,63 @@
+import { Prisma, PrismaPromise, Station } from "@prisma/client"
+//@ts-ignore - osm-read has no types
+import parseOsm from 'osm-pbf-parser'
+import through from 'through2'
+import fetch from 'node-fetch'
+
+import prisma from "../lib/prisma"
+
+const queries: PrismaPromise<Prisma.BatchPayload>[] = []
+queries.push(prisma.station.deleteMany({}))
+
+const newStations: Station[] = []
+const nodes: {[id: number]: any} = {}
+const osm = parseOsm()
+fetch('http://download.openstreetmap.fr/extracts/europe/czech_republic/praha-latest.osm.pbf')
+.then(rawData => rawData.body)
+.then(data => data?.pipe(osm))
+.then(data => {
+  console.log("Downloading OSM data...");
+  return data.pipe(through.obj((items, enc, next) => {
+    items.forEach((item: any) => {
+      if (item.type === 'node') {
+        if (item.tags.highway == 'bus_stop' || item.tags.railway == 'tram_stop' || item.tags.railway == 'halt' || item.tags.railway == 'station' || item.tags.railway == 'stop' || item.tags.public_transport == 'platform') {
+          nodes[item.id as number] = item
+        }
+      } /*else if (item.type === 'relation') {
+        //Not neded for now
+        if (item.tags.public_transport === 'stop_area') {
+          relations.push(item)
+        }
+      }*/
+    })
+    next()
+  }))
+})
+.then(data => {
+  return new Promise<Prisma.BatchPayload[]>((resolve) => {
+    data.on('finish', () => {
+      console.log("Parsing OSM data...")
+      const usedNames: Set<string> = new Set()
+      for (const nodeId of Object.keys(nodes)) {
+        const node = nodes[parseInt(nodeId)]
+        const name = node.tags.name
+        if (!name) continue;
+        if (usedNames.has(name)) continue;
+        const newStation = {
+          name: name,
+          lat: node.lat,
+          lon: node.lon,
+          id: node.id
+        }
+        newStations.push(newStation)
+        usedNames.add(name)
+      }
+      queries.push(prisma.station.createMany({data: newStations}))
+      console.log(`Parsing OSM data finished! Total stops: ${newStations.length}. Uploading to database...`)
+      resolve(prisma.$transaction(queries))
+    })
+  })
+})
+.then(() => {
+  console.log("Uploading finished!")
+})
--- server/stationOsmHandler.ts
+++ server/stationOsmHandler.ts
@@ -1,76 +1,22 @@
 import { StationData } from '../types/stationData'
-//@ts-ignore - osm-read has no types
-const parseOsm = require('osm-pbf-parser')
-import through from 'through2'
-import fetch from 'node-fetch'
-let stations: {[name: string]: StationData} = {}
+import prisma from '../lib/prisma'
 
-function ParseData() {
-  const newStations: {[name: string]: StationData} = {}
-  const relations: any[] = []
-  const nodes: {[id: number]: any} = {}
-  const osm = parseOsm()
-  return fetch('http://download.openstreetmap.fr/extracts/europe/czech_republic/praha-latest.osm.pbf')
-  .then(rawData => rawData.body)
-  .then(data => data?.pipe(osm))
-  .then(data => {
-    console.log("Downloading OSM data...");
-    return data.pipe(through.obj((items, enc, next) => {
-      items.forEach((item: any) => {
-        if (item.type === 'node') {
-          if (item.tags.highway == 'bus_stop' || item.tags.railway == 'tram_stop' || item.tags.railway == 'halt' || item.tags.railway == 'station' || item.tags.railway == 'stop') {
-            nodes[item.id as number] = item
-          }
-        } /*else if (item.type === 'relation') {
-          //Not neded for now
-          if (item.tags.public_transport === 'stop_area') {
-            relations.push(item)
-          }
-        }*/
-      })
-      next()
-    }))
+async function GetStationsCount(): Promise<number> {
+  return await prisma.station.count()
+}
+
+export async function GetRandomStation(): Promise<StationData> {
+  const stationCount = await GetStationsCount()
+  const station = await prisma.station.findFirst({
+    skip: Math.floor(Math.random() * stationCount)
   })
-  .then(data => {
-    return new Promise<void>((resolve) => {
-      data.on('finish', () => {
-        console.log("Parsing OSm data...")
-        for (const nodeId of Object.keys(nodes)) {
-          const node = nodes[parseInt(nodeId)]
-          const name = node.tags.name as string
-          const newStation = {
-            name: name,
-            coords: {
-              lat: node.lat,
-              lon: node.lon
-            },
-            nodeId: node.id
-          }
-          newStations[newStation.name] = newStation
-        }
-        stations = newStations
-        console.log(`Parsing OSM data finished! Total stops: ${GetStationsCount()}`)
-        resolve()
-      })
-    })
-  })
-}
 
-function GetStationsCount(): number {
-  return Object.keys(stations).length
-}
-
-function CheckParsingDone(): boolean {
-  return GetStationsCount() > 0
-}
-
-export async function GetRandomStation() {
-  if (!CheckParsingDone()) {
-    await ParseData()
+  return {
+    name: station?.name || "",
+    nodeId: station?.id || -1,
+    coords: {
+      lat: station?.lat || 0,
+      lon: station?.lon || 0
+    }
   }
-
-  const stationCount = GetStationsCount()
-
-  console.log(stationCount)
-  return stations[Object.keys(stations)[Math.floor(Math.random() * stationCount)]]
 }
